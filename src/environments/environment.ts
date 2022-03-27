1f9cfb56e8f5b359a6257e5c83a5827cd83cd82d
silvoneymachado <silvoneymachado@gmail.com>
2022-03-27 13:24:43 -0300

    Remove sensitive content

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://api.m3o.com/v1',
-  apiKey: 'MThlMmE5YjYtM2I1ZS00Mzg3LTkxMjMtYzg1YmViMGNkNzM1',
+  apiURL: '',
+  apiKey: '',
 };
 
 
