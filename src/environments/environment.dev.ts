8245af75761e087ef012bd49f5a3e06171106371
silvoneymachado <silvoneymachado@gmail.com>
2022-03-27 22:29:03 -0300

    Fix keys exposure

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiURL: 'https://api.m3o.com/v1',
+  authApiUrl: 'https://jwt-node.vercel.app',
+  apiKey: '',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://api.m3o.com/v1',
+  authApiUrl: 'https://jwt-node.vercel.app',
+  apiKey: '',
+};
+
