0b8923d82497c9bde4ea3d9c64e82f2088028f52
SILVONEY PINTO MACHADO <silvoneymachado@gmail.com>
2022-03-28 09:08:29 -0300

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-   production: true,
-   apiURL: "https://api.m3o.com/v1",
-   authApiUrl: "https://jwt-node.vercel.app",
-   apiKey: "ZDNmN2ZjYjQtMWE4Mi00ZjMwLTgwNDktYzYzYjEyNjU4ZWMy",
-};
